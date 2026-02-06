3e97ed1db66765f888547d86e408e5b0115c5c6c
ronbourges <bourgesron@mail.com>
2026-02-06 09:23:46 -0300

    User-data.json added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,16 +1,30 @@
+import userData from '../fixtures/userData.json'
+
+
 describe('OrangeHRM', () => {
-  it('passes', () => {
+
+   const selecctorList = {
+    usernameInput: "[name='username']",
+    passwordInput: "[name='password']",
+    submitButton: "button[type='submit']",
+    errorMessage: '.oxd-alert-content-text'
+
+   }
+   
+    it('passes', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('admin123')
-    cy.get("button[type='submit']").click()
+    cy.get(selecctorList.usernameInput).type(userData.passes.usernameInput)
+    cy.get(selecctorList.passwordInput).type(userData.passes.passwordInput)
+    cy.get(selecctorList.submitButton).click()
     cy.url().should('include', '/dashboard')
   })
+
   it('fails', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Test')
-    cy.get("[name='password']").type('test')
-    cy.get("button[type='submit']").click()
-    cy.get('.oxd-alert-content-text').should('contain', 'Invalid credentials')
+    
+    cy.get(selecctorList.usernameInput).type(userData.fails.usernameInput)
+    cy.get(selecctorList.passwordInput).type(userData.fails.passwordInput)
+    cy.get(selecctorList.submitButton).click()
+    cy.get(selecctorList.errorMessage).should('contain', 'Invalid credentials')
 })
 })
